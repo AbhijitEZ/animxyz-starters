011b57f48cdf1e973ea94550cc134a3c38ab2172
abhijitez <abhijit.nair659@gmail.com>
2021-03-13 15:25:37 +0530

    animxyz starters

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: { main: "./src/index.js" },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/public", "index.html"),
+      filename: "index.html",
+      inject: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/public", "contact.html"),
+      filename: "contact.html",
+      inject: true,
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
